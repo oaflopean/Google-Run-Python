f0194a6012c6b75d9faa7cf37e6df7b8bb41eeba
oaflopean <agnusdeI5>
2019-12-27 08:18:46 -0600

    Dec 27

---
+++ list.py
@@ -0,0 +1,29 @@
+from gutenberg.acquire import load_etext
+from gutenberg.cleanup import strip_headers
+import json
+
+file=json.loads(open("babelli-copypasta.json").read())
+number=1
+delete=open("list.txt", mode="w")
+delete.write("")
+delete.close()
+usb = open("list.txt", mode="a")
+
+for num in file.keys()[number-1:]:
+
+    #text = strip_headers(load_etext(int(num))).strip()
+    title=file[num]["title"]
+    try:
+        author=file[num]["author"].split(", ")[1]+" "+file[num]["author"].split(",")[0]
+    except IndexError:
+        author=file[num]["author"]
+    except AttributeError:
+        author="Unknown"
+    filename= title[:60] + " by " + author
+    filename=filename.replace("/","").replace("\"","").replace(":",";").replace(".",",").replace(",","").replace("?","").replace("!","") + ".txt"
+    #usb.write(text.encode('utf8'))
+    #usb.close()
+    print(str(number)+" "+filename)
+    usb.write(str(number)+ " " + filename.encode("utf8")+"\n")
+    number+=1
+usb.close()
